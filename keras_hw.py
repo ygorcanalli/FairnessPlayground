665c9b79cf50c92c309cb98698b97a399b318073
Ygor Canalli <ygor.canalli@gmail.com>
2019-04-25 16:37:40 -0300

    starting with keras

---
+++ keras_hw.py
@@ -0,0 +1,44 @@
+#%%
+from __future__ import absolute_import, division, print_function
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+#%%
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+#%%
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+#%%
+train_images.shape
+#%%
+len(train_labels)
+#%%
+train_labels
+
+#%%
+test_images.shape
+
+#%%
+len(test_labels)
+
+#%%
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+#%%
