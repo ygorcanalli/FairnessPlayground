4b83ee05257339f1674547660770b3b5dd00cc2e
Ygor Canalli <ygor.canalli@gmail.com>
2019-05-27 16:10:49 -0300

    evaluatign different fp fn rates

--- try_income.py
+++ try_income.py
@@ -44,11 +44,35 @@ X_test = test[:,:-2]
 y_test = test[:,-2:]
 
 #%% specifying model
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(108,)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(2, activation=tf.nn.softmax)
-])
+
+def create_model():
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(108,)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(2, activation=tf.nn.softmax)
+    ])
+
+    return model
+
+
+#%% pollute functions
+def pollute(data, T):
+    polluted_data = data.copy()
+    for i in range(data.size):
+        r = np.random.rand(1)
+        # if class is 0
+        if data[i,0] == 1:       
+            # probability of false positive
+            if r < T[0,1]:
+                polluted_data[i,0] = 0
+                polluted_data[i,1] = 1
+        # if class is 1
+        elif data[i,1] == 1:
+            # probability of false negative
+            if r < T[1,0]:
+                polluted_data[i,1] = 0
+                polluted_data[i,0] = 1
+    return polluted_data
 
 #%% custom loss function for forward
 
@@ -61,22 +85,58 @@ def forward_categorical_crossentropy(T):
    
     # Return a function
     return loss
-#%% non-sense transition matrix
 
-T = np.array([[0.4, 0.6],[0.65, 0.35]]).astype(np.float32)
+#%% defining evaluate function
 
+def evaluate(X_train, X_test, y_train, y_test, 
+                model_function=create_model,
+                polluted_y_data=None, traning_epochs=5,
+                loss_function=categorical_crossentropy):
 
-#%% specifying optmizer, loss and metrics
+    # initializing model
+    model = model_function()
 
-model.compile(optimizer='adam', 
-              loss=forward_categorical_crossentropy(T),
-              metrics=['accuracy'])
+    # specifying optmizer, loss and metrics
+    model.compile(optimizer='adam', 
+                loss=loss_function,
+                metrics=['accuracy'])
 
-#%% traning
-model.fit(X_train, y_train, epochs=5)
-#%% testing
-test_loss, test_acc = model.evaluate(X_test, y_test)
+    # disable data pollution
+    if polluted_y_data is None:
+        model.fit(X_train, y_train, epochs=traning_epochs)
+    # disable data pollution
+    else:
+        model.fit(X_train, polluted_y_train, epochs=traning_epochs)
+  
+    # testing with non polluted data
+    return model.evaluate(X_test, y_test)
 
-print('Test accuracy:', test_acc)
-#%% making prediction
-predictions = model.predict(X_test)
+#%% baseline
+test_loss, test_acc = evaluate(X_train, X_test, y_train, y_test,
+                                polluted_y_data=None, loss_function=categorical_crossentropy)
+print('Baseline test accuracy:', test_acc)
+
+#%% evaluating different error rates
+
+false_positive_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+false_negative_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+
+for fp in false_negative_rates:
+    for fn in false_negative_rates:
+        print("fp_rate: ", fp, " fn_rate: ", fn)
+        T = np.array([[1-fp, fp],
+                      [fn  , 1-fn]]).astype(np.float32)
+
+        polluted_labels = pollute(y_train, T)
+        forward_function = forward_categorical_crossentropy(T)
+
+        # polluted data without forward
+        test_loss, test_acc = evaluate(X_train, X_test, y_train, y_test,
+                                        polluted_y_data=polluted_labels,
+                                        loss_function=categorical_crossentropy)
+        print('Test accuracy without forward:', test_acc)
+        # polluted data with forward
+        test_loss, test_acc = evaluate(X_train, X_test, y_train, y_test,
+                                        polluted_y_data=polluted_labels,
+                                        loss_function=forward_function)
+        print('Test accuracy with forward:', test_acc)
